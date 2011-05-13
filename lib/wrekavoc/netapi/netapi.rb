09682da2069d033451dc0a14993c1a68f7014dba
Luc Sarzyniec <mail@olbat.net>
2011-05-13 15:04:51 +0200

    Add comments to specify methods which are only for Daemons or for Nodes

--- lib/wrekavoc/netapi/netapi.rb
+++ lib/wrekavoc/netapi/netapi.rb
@@ -9,11 +9,11 @@ module Wrekavoc
     VNODE_START='/vnodes/start'
     VNODE_STOP='/vnodes/stop'
     VIFACE_CREATE='/vnodes/vifaces/create'
-    VIFACE_ATTACH='/vnodes/vifaces/attach'
+    VIFACE_ATTACH='/vnodes/vifaces/attach' # Node only
     VNODE_INFO_ROOTFS='/vnodes/info/rootfs'
     
-    VNETWORK_CREATE='/vnetworks/create'
-    VNETWORK_ADD_VNODE='/vnetworks/vnodes/add'
+    VNETWORK_CREATE='/vnetworks/create' # Daemon only
+    VNETWORK_ADD_VNODE='/vnetworks/vnodes/add' # Daemon only
     VNETWORK_INFO_LIST='/vnetwork/info/list'
 
   end
