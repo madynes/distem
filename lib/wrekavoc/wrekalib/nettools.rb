f85e41df980ababf0beb74c7343500283e6b0c79
Luc Sarzyniec <mail@olbat.net>
2011-05-31 15:23:10 +0200

    Add default address cache

--- lib/wrekavoc/wrekalib/nettools.rb
+++ lib/wrekavoc/wrekalib/nettools.rb
@@ -6,9 +6,10 @@ module Wrekavoc
     class NetTools
       NAME_BRIDGE='br0'
       @@nic_count=1
+      @@addr_default=nil
 
       def self.get_default_iface
-        cmdret = Lib::Shell.run("/sbin/route") 
+        cmdret = Shell.run("/sbin/route") 
         # | grep 'default' | awk '{print \$8}' | tr -d '\n'")
         ret=""
         cmdret.each_line { |s| ret=s.split[7] if s.include?("default") }
@@ -16,41 +17,48 @@ module Wrekavoc
       end 
 
       def self.get_iface_addr(iface)
-        cmdret = Lib::Shell.run("/sbin/ifconfig #{iface}") 
+        cmdret = Shell.run("/sbin/ifconfig #{iface}") 
         # | grep 'inet addr' | awk '{print \$2}' | cut -d':' -f2 | tr -d '\n'")
         ret=""
         cmdret.each_line { |s| ret=s.split[1].split(":")[1] if s.include?("inet addr") }
         return ret
       end
 
-      def self.get_default_addr
-        iface = self.get_default_iface()
-        self.get_iface_addr(iface).strip
+      def self.get_default_addr(cache=true)
+        if !@@addr_default or !cache
+          iface = self.get_default_iface()
+          @@addr_default = self.get_iface_addr(iface).strip
+        end
+        return @@addr_default
       end
 
       def self.set_bridge
         iface = self.get_default_iface()
         addr = self.get_default_addr()
 
-        str = Lib::Shell.run("ifconfig")
+        str = Shell.run("ifconfig")
 
         unless str.include?("#{NAME_BRIDGE}")
-          Lib::Shell.run("brctl addbr #{NAME_BRIDGE}")
-          Lib::Shell.run("brctl setfd #{NAME_BRIDGE} 0")
-          Lib::Shell.run("ifconfig #{NAME_BRIDGE} #{addr} promisc up")
-          Lib::Shell.run("brctl addif #{NAME_BRIDGE} #{iface}")
-          Lib::Shell.run("ifconfig #{iface} 0.0.0.0 up")
+          Shell.run("brctl addbr #{NAME_BRIDGE}")
+          Shell.run("brctl setfd #{NAME_BRIDGE} 0")
+          Shell.run("ifconfig #{NAME_BRIDGE} #{addr} promisc up")
+          Shell.run("brctl addif #{NAME_BRIDGE} #{iface}")
+          Shell.run("ifconfig #{iface} 0.0.0.0 up")
           iface = self.get_default_iface()
           unless iface.empty?
-            Lib::Shell.run("ip route del default dev #{iface}")
+            Shell.run("ip route del default dev #{iface}")
           end
-          Lib::Shell.run("ip route add default dev #{NAME_BRIDGE}")
+          Shell.run("ip route add default dev #{NAME_BRIDGE}")
         end
+      end
+
+      def self.set_ifb(nb=8)
+        Shell.run("modprobe ifb numifbs=#{nb}")
       end
 
       def self.set_new_nic(address)
         iface = self.get_default_iface()
-        Lib::Shell.run("ifconfig #{iface}:#{@@nic_count} #{address}")
+        Shell.run("ifconfig #{iface}:#{@@nic_count} #{address}")
         @@nic_count += 1
       end
 
