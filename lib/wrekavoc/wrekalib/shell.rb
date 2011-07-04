62e2629493f7e639959f8f7d19faa609dcb2d929
Luc Sarzyniec <mail@olbat.net>
2011-07-04 15:15:29 +0200

    Add stderr in Shell.run logs

--- lib/wrekavoc/wrekalib/shell.rb
+++ lib/wrekavoc/wrekalib/shell.rb
@@ -1,20 +1,36 @@
+require 'open3'
+
 module Wrekavoc
   module Lib
 
     class Shell
 
       PATH_WREKAD_LOG_CMD=File.join(FileManager::PATH_WREKAVOC_LOGS,"wrekad.cmd")
-      def self.run(cmd)
+      def self.run(cmd, simple=false)
         cmdlog = "(#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}) #{cmd}"
-        ret = `#{cmd}`
-        raise ShellError.new(cmd,ret) unless $?.success?
 
-        retlog = ""
-        ret.each_line { |line| retlog += "  #{line}" }
-        Dir::mkdir(FileManager::PATH_WREKAVOC_LOGS) \
-          unless File.exists?(FileManager::PATH_WREKAVOC_LOGS)
-        File.open(PATH_WREKAD_LOG_CMD,'a+') { |f| f.write("#{cmdlog}\n#{retlog}") }
-      
+        ret = ""
+        log = ""
+        error = false
+
+        if simple
+          ret = `#{cmd}`
+          log = "#{cmdlog}\n#{ret}"
+          error = !$?.success?
+        else
+          Open3.popen3(cmd) do |stdin, stdout, stderr|
+            ret = stdout.read
+            err = stderr.read
+            Dir::mkdir(FileManager::PATH_WREKAVOC_LOGS) \
+              unless File.exists?(FileManager::PATH_WREKAVOC_LOGS)
+            log = "#{cmdlog}\n#{ret}"
+            log += "\nError: #{err}" unless err.empty? 
+            error = !$?.success? or !err.empty?
+          end
+        end
+        File.open(PATH_WREKAD_LOG_CMD,'a+') { |f| f.write(log) }
+        raise ShellError.new(cmd,ret,err) if error
+
         return ret
       end
     end
