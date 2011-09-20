b2a18b1780d8037de75cb060c228816128449f91
Luc Sarzyniec <luc.sarzyniec@gmail.com>
2011-09-20 17:28:48 +0200

    Save lxc logs in /var/log/distem/lxc

--- lib/distem/wrapper/lxc/configfile.rb
+++ lib/distem/wrapper/lxc/configfile.rb
@@ -1,21 +1,24 @@
+require 'distem'
+
 module LXCWrapper # :nodoc: all
 
   class ConfigFile
-    DEFAULT_DEV_RULES="
-      lxc.tty = 4
-      lxc.pts = 1024
-      lxc.cgroup.devices.deny = a
-      lxc.cgroup.devices.allow = c 1:3 rwm  # /dev/null
-      lxc.cgroup.devices.allow = c 1:5 rwm  # /dev/zero
-      lxc.cgroup.devices.allow = c 5:1 rwm  # /dev/console
-      lxc.cgroup.devices.allow = c 5:0 rwm  # /dev/tty
-      lxc.cgroup.devices.allow = c 4:0 rwm  # /dev/tty0
-      lxc.cgroup.devices.allow = c 4:1 rwm  # /dev/tty1
-      lxc.cgroup.devices.allow = c 1:8 rwm  # /dev/random
-      lxc.cgroup.devices.allow = c 1:9 rwm  # /dev/urandom
-      lxc.cgroup.devices.allow = c 136:* rwm  # /dev/pts/*
-      lxc.cgroup.devices.allow = c 5:2 rwm  #  /dev/pts/ptmx
-      lxc.cgroup.devices.allow = c 254:0 rwm  # rtc"  
+    PATH_LOG_DIR=File.join(Distem::Lib::FileManager::PATH_DISTEM_LOGS,'lxc')
+    DEFAULT_DEV_RULES=""\
+      "lxc.tty = 4" \
+      "lxc.pts = 1024" \
+      "lxc.cgroup.devices.deny = a" \
+      "lxc.cgroup.devices.allow = c 1:3 rwm  # /dev/null" \
+      "lxc.cgroup.devices.allow = c 1:5 rwm  # /dev/zero" \
+      "lxc.cgroup.devices.allow = c 5:1 rwm  # /dev/console" \
+      "lxc.cgroup.devices.allow = c 5:0 rwm  # /dev/tty" \
+      "lxc.cgroup.devices.allow = c 4:0 rwm  # /dev/tty0" \
+      "lxc.cgroup.devices.allow = c 4:1 rwm  # /dev/tty1" \
+      "lxc.cgroup.devices.allow = c 1:8 rwm  # /dev/random" \
+      "lxc.cgroup.devices.allow = c 1:9 rwm  # /dev/urandom" \
+      "lxc.cgroup.devices.allow = c 136:* rwm  # /dev/pts/*" \
+      "lxc.cgroup.devices.allow = c 5:2 rwm  #  /dev/pts/ptmx" \
+      "lxc.cgroup.devices.allow = c 254:0 rwm  # rtc"  
 
     def self.generate(vnode,filepath)
       # Write lxc config file
@@ -27,13 +30,17 @@ module LXCWrapper # :nodoc: all
         else
           f.puts "lxc.rootfs = #{vnode.filesystem.path}"
         end
-        f.puts "# mounts point"
+
+        Distem::Lib::Shell.run("mkdir -p #{PATH_LOG_DIR}") \
+          unless File.directory?(PATH_LOG_DIR)
+        
         f.puts "lxc.mount.entry=proc #{vnode.filesystem.path}/proc " \
           "proc nodev,noexec,nosuid 0 0"
         f.puts "lxc.mount.entry=devpts #{vnode.filesystem.path}/dev/pts " \
           "devpts defaults 0 0"
         f.puts "lxc.mount.entry=sysfs #{vnode.filesystem.path}/sys " \
           "sysfs defaults  0 0"
+        f.puts "lxc.console = #{File.join(PATH_LOG_DIR,vnode.name)}"
 
         vnode.vifaces.each do |viface|
           f.puts "lxc.network.type = veth"
