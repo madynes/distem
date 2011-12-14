madynes/distem

commit 157f5cdaf114e70ead9d682722d84c6c1e4ad2f2

Update ssh_key management, copying authorized_key from host and fix the bug of non-copied custom key

lib/distem/node/container.rb
@@ -10,7 +10,7 @@ module Distem
       MAX_SIMULTANEOUS_ACTIONS = 32
       MAX_SIMULTANEOUS_CONFIG = 32
       # The prefix to set to the SSH key whick are copied on the virtual nodes
-      SSH_KEY_PREFIX = 'distemkey-'
+      SSH_KEY_PREFIX = ''
       # The default file to save virtual node specific ssh key pair (see Distem::Resource::VNode::sshkey)
       SSH_KEY_FILENAME = 'identity'
 
@@ -94,8 +94,21 @@ module Distem
           f.puts @vnode.sshkey['public']
         end if @vnode.sshkey and @vnode.sshkey['public']
 
-        # Adding public keys to SSH authorized_keys file
+        # Copying authorized_keys file of the host
+        hostauthfile = File.join(Daemon::Admin::PATH_SSH,'authorized_keys')
         authfile = File.join(sshpath,'authorized_keys')
+        authkeys = IO.readlines(authfile).collect{|v| v.chomp}
+        if File.exists?(authfile)
+          hostauthkeys = IO.readlines(authfile).collect{|v| v.chomp}
+          hostauthkeys.each do |key|
+            File.open(authfile,'a') { |f| f.puts key } unless \
+              authkeys.include?(key)
+          end
+        else
+          Lib::Shell.run("cp -f #{hostauthfile} #{authfile}") if File.exists?(hostauthfile)
+        end
+
+        # Adding public keys to SSH authorized_keys file
         pubkeys = Daemon::Admin.ssh_keys_pub.collect{ |v| IO.read(v).chomp }
         pubkeys << @vnode.sshkey['public'] if @vnode.sshkey and @vnode.sshkey['public']
         if File.exists?(authfile)

lib/distem/daemon/admin.rb
@@ -11,33 +11,33 @@ module Distem
       # Default SSH user
       SSH_USER=ENV['USER']
       # Paths to the SSH key files
-      PATH_SSH_KEYS=File.join(File.expand_path("~#{SSH_USER}"),'.ssh')
+      PATH_SSH=File.join(File.expand_path("~#{SSH_USER}"),'.ssh')
 
-      # Get current user SSH public keys paths (looking in PATH_SSH_KEYS directory)
+      # Get current user SSH public keys paths (looking in PATH_SSH directory)
       # ==== Returns
       # Array of String object
       def self.ssh_keys_pub
         ret = []
-        begin 
-          ret = Dir.entries(PATH_SSH_KEYS).select {|v| v =~ /^id.*\.pub$/} \
-            if File.directory?(PATH_SSH_KEYS)
+        begin
+          ret = Dir.entries(PATH_SSH).select {|v| v =~ /^id.*\.pub$/} \
+            if File.directory?(PATH_SSH)
         rescue ArgumentError
         end
-        return ret.collect!{|v| File.join(PATH_SSH_KEYS,v)}
+        return ret.collect!{|v| File.join(PATH_SSH,v)}
       end
 
-      # Get current user SSH private keys paths (looking in PATH_SSH_KEYS directory)
+      # Get current user SSH private keys paths (looking in PATH_SSH directory)
       # ==== Returns
       # Array of String object
       def self.ssh_keys_priv
         ret = []
-        begin 
-          ret = Dir.entries(PATH_SSH_KEYS).select do |v|
+        begin
+          ret = Dir.entries(PATH_SSH).select do |v|
             v =~ /^id.*$/ and File.extname(v) =~  /^(?!\.pub)$/
-          end if File.directory?(PATH_SSH_KEYS)
+          end if File.directory?(PATH_SSH)
         rescue ArgumentError
         end
-        return ret.collect!{|v| File.join(PATH_SSH_KEYS,v)}
+        return ret.collect!{|v| File.join(PATH_SSH,v)}
       end
 
       # Run a daemon on a distant server (physical node)

lib/distem/daemon/distemdaemon.rb
@@ -307,6 +307,9 @@ module Distem
 
         downkeys(desc)
 
+        vnode.sshkey = desc['ssh_key'] if desc['ssh_key'] and \
+          (desc['ssh_key'].is_a?(Hash) or desc['ssh_key'].nil?)
+
         vnode_attach(vnode.name,desc['host']) if desc['host']
 
         vfilesystem_create(vnode.name,desc['vfilesystem']) if desc['vfilesystem']

lib/distem/topologystore/hashwriter.rb
@@ -50,11 +50,11 @@ module Distem
         end
 
         if vnode.sshkey
-          ret['sshkey'] = {}
-          ret['sshkey']['public'] = vnode.sshkey['public'] if vnode.sshkey['public']
-          ret['sshkey']['private'] = vnode.sshkey['private'] if vnode.sshkey['private']
+          ret['ssh_key'] = {}
+          ret['ssh_key']['public'] = vnode.sshkey['public'] if vnode.sshkey['public']
+          ret['ssh_key']['private'] = vnode.sshkey['private'] if vnode.sshkey['private']
         else
-          ret['sshkey'] = nil
+          ret['ssh_key'] = nil
         end
 
         return ret

commit 44f5b3d6b471302bf0aef9377260fd22d663c4cc

fixes problem introduced in 157f5cdaf114e70ead9d682722d84c6c1e4ad2f2 the authorized_keys was read even though it could not exist

lib/distem/node/container.rb
@@ -97,8 +97,8 @@ module Distem
         # Copying authorized_keys file of the host
         hostauthfile = File.join(Daemon::Admin::PATH_SSH,'authorized_keys')
         authfile = File.join(sshpath,'authorized_keys')
-        authkeys = IO.readlines(authfile).collect{|v| v.chomp}
         if File.exists?(authfile)
+          authkeys = IO.readlines(authfile).collect{|v| v.chomp}
           hostauthkeys = IO.readlines(authfile).collect{|v| v.chomp}
           hostauthkeys.each do |key|
             File.open(authfile,'a') { |f| f.puts key } unless \