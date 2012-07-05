4acc36825b4334c2e104cce18d48e02402c1dd0c
Jean-Baptiste Hervé <jean-baptiste.herve@esial.net>
2012-07-05 17:38:45 +0200

    EventGenerator : Add code to manage the generation of random values and to add the next event to the list
    It only support the uniform probabilist distribution for now; anyway, this is not a problem because you cannot instanciate it for the moment! :P

--- lib/distem/events/eventgenerator.rb
+++ lib/distem/events/eventgenerator.rb
@@ -6,27 +6,36 @@ module Distem
     # Class to generate events from a probabilist distribution
     class EventGenerator < Event
 
-      def initialize(resource_desc, change_type, generator_desc, event_value = nil)
+      def initialize(resource_desc, change_type, generator_desc, event_value = nil, value_generator = nil, date_generator = nil)
+
+        @generator_desc = generator_desc
+        @random_generators = {}
+        raise "No description given for the date generator" unless @generator_desc['date']
 
         if change_type == 'churn'
           event_value = 'down' unless event_value
         else
-          raise "No description given for the value generator" unless generator_desc['value']
-          @value_generator = SimpleRandomGenerator.new
-          @value_generator_params = generator_desc['value']
-          event_value = get_random_value(@value_generator, @value_generator_params)
+          raise "No description given for the value generator" unless @generator_desc['value']
+          if value_generator
+            @random_generators['value'] = value_generator
+          else
+            @random_generators['value'] = SimpleRandomGenerator.new
+          end
+          event_value = get_random_value('value')
         end
 
         super(resource_desc, change_type, event_value)
 
-        raise "No description given for the date generator" unless generator_desc['date']
-        @date_generator = SimpleRandomGenerator.new
-        @date_generator_params = generator_desc['date']
+        if date_generator
+          @random_generators['date'] = date_generator
+        else
+          @random_generators['date'] = SimpleRandomGenerator.new
+        end
 
       end
 
       def get_next_date
-        # TODO generate a new date for the next event
+        return get_random_value('date')
       end
 
       def trigger(event_list)
@@ -39,11 +48,24 @@ module Distem
       protected
 
       def get_next_event
-        # TODO create a new EventGenerator, with a new value
+        next_value = nil
+        if @change_type == 'churn'
+          next_value = (@event_value == 'up' ? 'down' : 'up')
+        end
+        return EventGenerator.new(@resource_desc, @change_type, @generator_desc, next_value, @random_generators['value'], @random_generators['date'])
       end
 
-      def get_random_value(random_generator, generator_parameters)
-        # TODO generate a new random value, given the generator parameters
+      # Get a value for the choosen generator, 'date' or 'value'
+      def get_random_value(generator)
+        generator_parameters = @generator_desc[generator]
+        random_generator = @random_generators[generator]
+
+        if generator_parameters['distribution'] == 'uniform'
+          return generator_parameters['min'].to_f +
+              (generator_parameters['max'].to_f - generator_parameters['min'].to_f) * random_generator.rand_U01
+        else
+          raise "Probabilist distribution not supported : #{generator_parameters['distribution']}"
+        end
       end
 
     end
