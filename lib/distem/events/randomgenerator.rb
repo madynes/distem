4e2621295f516eb4aa0a8488944e4ca0637393b1
Jean-Baptiste Hervé <jean-baptiste.herve@esial.net>
2012-07-05 11:22:05 +0200

    Add a simple (and bad) random generator, which will be used to generate events

---
+++ lib/distem/events/randomgenerator.rb
@@ -0,0 +1,16 @@
+module Distem
+  module Events
+    class RandomGenerator
+
+      def initialize(seed)
+      end
+
+      def rand_U01
+      end
+
+      def advance_state(displacement)
+      end
+
+    end
+  end
+end
---
+++ lib/distem/events/simplerandomgenerator.rb
@@ -0,0 +1,30 @@
+module Distem
+  module Events
+    class SimpleRandomGenerator < RandomGenerator
+
+      # Class which badly generates random numbers
+      # All the instances of this class use the same random generator,
+      # embedded in the Kernel module
+      # This should not be used.
+
+      def initialize(seed)
+        # Set the seed for everyone uses this class (!)
+        if seed.is_a?(Array)
+          tmp_seed = 0
+          seed.each do |x| tmp_seed += x end
+          seed = tmp_seed
+        end
+        srand(seed)
+      end
+
+      def rand_U01
+        return rand
+      end
+
+      def advance_state(displacement)
+        # Not supported, so, does nothing...
+      end
+
+    end
+  end
+end
