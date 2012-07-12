274911b0898718f0005e31bc14675a538adb2a2b
Jean-Baptiste Hervé <jean-baptiste.herve@esial.net>
2012-07-12 13:46:24 +0200

    Add the possibility to give distinct availability and unavailability distribution
    Distribution can now be set to 'always', especially for availability and unavailability, to make a state last forever.

--- lib/distem/events/eventgenerator.rb
+++ lib/distem/events/eventgenerator.rb
@@ -11,11 +11,30 @@ module Distem
         @generator_desc = generator_desc
         @random_generators = {}
         @random_generators = random_generators if random_generators
-        raise "No description given for the date generator" unless @generator_desc['date']
 
         if change_type == 'churn'
           event_value = 'down' unless event_value
+
+          if @generator_desc['date']
+            if @generator_desc['availability'] or @generator_desc['unavailability']
+              raise "You cannot give a date generator and an availability or unavailability generator"
+            end
+
+            # same distribution for avaibility and unavailability time
+            @generator_desc['availability'] = @generator_desc['date']
+            @generator_desc['unavailability'] = @generator_desc['date']
+            @generator_desc['date'] = nil
+          else
+            unless @generator_desc['availability'] and @generator_desc['unavailability']
+              raise "You must give availability and unavailability generators"
+            end
+          end
+
+          @random_generators['availability'] = RngStreamRandomGenerator.new unless @random_generators['availability']
+          @random_generators['unavailability'] = RngStreamRandomGenerator.new unless @random_generators['unavailability']
+
         else
+          raise "No description given for the date generator" unless @generator_desc['date']
           raise "No description given for the value generator" unless @generator_desc['value']
 
           @random_generators['value'] = RngStreamRandomGenerator.new unless @random_generators['value']
@@ -38,14 +57,27 @@ module Distem
       end
 
       def get_next_date
-        return get_random_value('date')
+        if @change_type == 'churn'
+          if @event_value == 'down'
+            # The resource will be down on the next trigger; for the moment, it should be up.
+            # So we use the availability distribution to compute the next date
+            return get_random_value('availability')
+          else
+            return get_random_value('unavailability')
+          end
+        else
+          return get_random_value('date')
+        end
       end
 
       def trigger(event_list, date)
         super
-        next_event = get_next_event
-        next_date = get_next_date + date
-        event_list.add_event(next_date, next_event)
+        generated_date = get_next_date
+        if generated_date
+          next_event = get_next_event
+          next_date = generated_date + date
+          event_list.add_event(next_date, next_event)
+        end
       end
 
       protected
@@ -72,6 +104,12 @@ module Distem
 
         elsif generator_parameters['distribution'] == 'weibull'
           return generator_parameters['scale'] * ( (-Math::log(random_generator.rand_U01)) ** (1.0 / generator_parameters['shape'].to_f) )
+
+        elsif generator_parameters['distribution'] == 'always'
+          # This case could be used for churn, when one wants a resource
+          # to be up or down forever after the previous trigger.
+          # Thus, no event will be set, so we return a nil date
+          return nil
 
         else
           raise "Probabilist distribution not supported : #{generator_parameters['distribution']}"
