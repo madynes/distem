f004e811e9326bfdfb56bb62aa862fc4143d7a81
Emmanuel Jeanvoine <emmanuel.jeanvoine@inria.fr>
2013-05-17 15:32:48 +0200

    Improved a bit the test suite.
    Change-Id: Iab2b0b25f6972e4b34f46ca53383a414eb1020b0

--- test/experimental_testing/exps/exp-bandwidth.rb
+++ test/experimental_testing/exps/exp-bandwidth.rb
@@ -5,8 +5,9 @@ require File.join(File.dirname(__FILE__), 'stats')
 require File.join(File.dirname(__FILE__), 'helpers')
 
 ERROR = ARGV[0].to_f / 100
+WAY = ARGV[1]
 IPFILE = '/tmp/ip'
-REPETS = 10
+REPETS = 3
 
 puts '<<< Bandwidth test >>>'
 
@@ -15,15 +16,15 @@ ip = IO.readlines(IPFILE).collect { |line| line.strip }
 def run_exp(band, f, ip)
   ret = true
 
-  run_and_wait('distem --execute vnode=node1,command="iperf -s -P #{REPETS}"') do 
+  run_and_wait("distem --execute vnode=node1,command=\"iperf -s -P #{REPETS}\"") do
     sleep 1  # wait 1 second for iperf-server to start
     nums = Stats.new
-    REPETS.times {  # repeat the measurement repets time
-      data = `distem --execute vnode=node2,command="iperf -t 2 -y c -c #{ip[0]}"`
+    REPETS.times { |i|  # repeat the measurement repets time
+      data = `distem --execute vnode=node2,command="iperf -t 3 -y c -c #{ip[0]}"`
       result = data.split(',').last.to_f / 1e6
+      puts result
       nums.push result
     }
-
     if ((nums.mean) > ((1 + ERROR) * band / f)) || ((nums.mean) < ((1 - ERROR) * band / f))
       puts "ERROR: requested #{band / f}mbits, measured #{nums.mean}mbits"
       ret = false
@@ -33,37 +34,40 @@ def run_exp(band, f, ip)
 end
 
 error = false
+ifnet = {}
 Distem.client do |cl|
-  
-  cl.reset_emulation
-  
-  bands2 = [ 56, 128, 256, 512, 768, 1024 ]
+  bands2 = [ 256, 512, 1024 ]
   bands2.each { |band|
-    ifnet = { 'input' => {}, 'output' => {} }
-    cl.viface_update 'node2', 'af0', ifnet
-
-    ifnet['input'] = { 'bandwidth' => {'rate' => "#{band}kbit"} }
-    cl.viface_update 'node1', 'af0', ifnet
+    if WAY == 'input'
+      ifnet['input'] = { 'bandwidth' => {'rate' => "#{band}kbit"} }
+      cl.viface_update 'node1', 'af0', ifnet
+    else
+      ifnet['output'] = { 'bandwidth' => {'rate' => "#{band}kbit"} }
+      cl.viface_update 'node2', 'af0', ifnet
+    end
 
     if not run_exp(band, 1000.0, ip)
       error = true
       break
     end
   }
-  
-  bands = [ 10, 100, 300, 500, 700, 900 ]  # this is in megabits/s
-  not error && bands.each { |band|
-    ifnet = { 'input' => {}, 'output' => {} }
-    cl.viface_update 'node2', 'af0', ifnet
-    
-    ifnet['input'] = { 'bandwidth' => {'rate' => "#{band}mbit"} }
-    cl.viface_update 'node1', 'af0', ifnet
-    
-    if not run_exp(band, 1.0, ip)
-      error = true
-      break
-    end
-  }
+  bands = [ 10, 50, 100, 500, 900 ]  # this is in megabits/s
+  if not error
+    bands.each { |band|
+      if WAY == 'input'
+        ifnet['input'] = { 'bandwidth' => {'rate' => "#{band}mbit"} }
+        cl.viface_update 'node1', 'af0', ifnet
+      else
+        ifnet['output'] = { 'bandwidth' => {'rate' => "#{band}mbit"} }
+        cl.viface_update 'node2', 'af0', ifnet
+      end
+
+      if not run_exp(band, 1.0, ip)
+        error = true
+        break
+      end
+    }
+  end
 end
 if error
   puts 'TEST NOT PASSED'
